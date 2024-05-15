f59b5b945007ae4db2b30c837eaacc7dfb6b0c52
Davinder Singh <sin14349@sheridancollege.ca>
2024-05-15 15:34:22 -0500

    initialising commit

---
+++ src/javaapplication1/Student.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author davin
+ */
+class Student {
+
+    private String name;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param givenName the name to set
+     */
+    public void setName(String givenName) {
+        name = givenName;
+    }
+}
